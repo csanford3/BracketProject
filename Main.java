6673314960023cf481544b6d568e05a4bceb545f
csanford3 <37089800+csanford3@users.noreply.github.com>
2018-04-21 16:06:03 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -56,7 +57,12 @@ public class Main extends Application {
 			
 			//Gridpane shich allows flexibility
 			GridPane grid = new GridPane();
+			
+			//Padding sets space around edge of window
 			grid.setPadding(new Insets(10, 10, 10, 10));
+			
+			//Alignment of window set to the top and centered
+			grid.setAlignment(Pos.TOP_CENTER);
 			grid.setMinSize(300, 300);
 			grid.setVgap(5);
 			grid.setHgap(5);
@@ -66,37 +72,46 @@ public class Main extends Application {
 
 			TextField text1 = new TextField();
 			text1.setPrefColumnCount(10);
+			text1.setPrefSize(5, 5);
 			grid.add(text1, 1, 0);
 
 			Text team2 = new Text("Team 2");
-			grid.add(team2, 0, 1);
+			grid.add(team2, 0, 2);
 
 			TextField text2 = new TextField();
 			text2.setPrefColumnCount(10);
-			grid.add(text2, 1, 1);
+			grid.add(text2, 1, 2);
 			
 			Text team3 = new Text("Team 3");
-			grid.add(team3, 4, 0);
+			grid.add(team3, 6, 0);
 			
 			TextField text3 = new TextField();
 			text3.setPrefColumnCount(10);
-			grid.add(text3, 3, 0);
+			grid.add(text3, 5, 0);
 			
 			
 			
-			Team team4 = new Team("Team4");
+			Team team4 = new Team("Team 4 \n China");
 
 			
-			grid.add(team4.text, 5, 1);
+			grid.add(team4.text, 6, 2);
 			
 			
 			team4.textField.setPrefColumnCount(10);
-			grid.add(team4.textField, 4, 1);
+			grid.add(team4.textField, 5, 2);
 			
+			
+			Team team5 = new Team("Team 5");
+			grid.add(team5.text, 5, 1);
+			
+			team5.textField.setPrefColumnCount(10);
+			grid.add(team5.textField,4, 1);
 			
 			
 			grid.setStyle("-fx-background-color: #F8BFD1;");
 			
+			
+			//BorderPane containing flow and grid pane as elements
 			BorderPane pane = new BorderPane();
 			pane.setTop(flow);
 			pane.setCenter(grid);
